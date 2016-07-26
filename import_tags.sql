d2025a4b4e46c74d71d2a1a41bac27ccdbd6ad50
Mario Pérez Esteso <mario@geekytheory.com>
2016-07-26 21:56:34 +0200

    Import relationships between posts and tags

--- import_tags.sql
+++ import_tags.sql
@@ -1,5 +1,4 @@
 -- Import tags from wp_terms, wp_term_taxonomy and wp_term_relationships
-
 INSERT INTO laraweb.tags (tag, slug, description)
   SELECT
     wpterms.name,
@@ -9,3 +8,19 @@ INSERT INTO laraweb.tags (tag, slug, description)
     JOIN wordpressdb.wp_term_taxonomy AS wptermtaxonomy ON wpterms.term_id = wptermtaxonomy.term_id
   WHERE wptermtaxonomy.taxonomy = 'post_tag';
 
+-- Import relationships between posts and tags
+INSERT INTO laraweb.posts_tags (post_id, tag_id)
+  SELECT
+    larawebposts.id,
+    larawebtags.id
+  FROM
+    laraweb.posts as larawebposts,
+    laraweb.tags as larawebtags,
+    wordpressdb.wp_term_relationships as wptermrelationships
+    JOIN wordpressdb.wp_term_taxonomy AS wptermtaxonomy ON wptermtaxonomy.term_taxonomy_id = wptermrelationships.term_taxonomy_id
+    JOIN wordpressdb.wp_terms AS wpterms ON wpterms.term_id = wptermtaxonomy.term_id
+    JOIN wordpressdb.wp_posts AS wpposts ON wpposts.ID = wptermrelationships.object_id
+  WHERE wptermtaxonomy.taxonomy = 'post_tag'
+    AND larawebposts.slug = wpposts.post_name COLLATE utf8_unicode_ci
+    AND larawebposts.slug <> ''
+    AND larawebtags.slug = wpterms.slug;
