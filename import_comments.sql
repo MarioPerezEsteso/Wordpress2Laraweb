5dbd31c776bcd30e61d71ad19ad3f3e7d6f9b67b
Mario Pérez Esteso <mario@geekytheory.com>
2016-10-09 14:26:15 +0200

    Insert comments with parents

--- import_comments.sql
+++ import_comments.sql
@@ -1,22 +1,48 @@
 -- Insert comments without parent
 
-INSERT INTO laraweb.comments (post_id, user_id, parent, author_name, author_email, author_url, body, upvotes, downvotes, approved, spam, ip, created_at, updated_at)
+INSERT INTO laraweb.comments (id, post_id, user_id, parent, author_name, author_email, author_url, body, upvotes, downvotes, approved, spam, ip, created_at, updated_at)
   SELECT
-    larawebposts.id, -- comments.id
-    null, -- comments.user_id
-    null, -- comments.parent
-    wpcomments.comment_author, -- comments.author_name
-    wpcomments.comment_author_email, -- comments.author_email
-    wpcomments.comment_author_url, -- comments.author_url
-    wpcomments.comment_content, -- comments.body
-    0, -- comments.upvotes
-    0, -- comments.downvotes
-    IF(wpcomments.comment_approved = 'spam', 0, 1), -- comments.approved
-    IF(wpcomments.comment_approved = 'spam', 1, 0), -- comments.spam
-    wpcomments.comment_author_IP, -- comments.ip
-    wpcomments.comment_date, -- comments.created_at
-    wpcomments.comment_date -- comments.created_at
+    wpcomments.comment_ID,
+    larawebposts.id,
+    NULL,
+    NULL,
+    wpcomments.comment_author,
+    wpcomments.comment_author_email,
+    wpcomments.comment_author_url,
+    wpcomments.comment_content,
+    0,
+    0,
+    IF(wpcomments.comment_approved = 'spam', 0, 1),
+    IF(wpcomments.comment_approved = 'spam', 1, 0),
+    wpcomments.comment_author_IP,
+    wpcomments.comment_date,
+    wpcomments.comment_date
   FROM wordpressdb.wp_comments AS wpcomments
     JOIN wordpressdb.wp_posts AS wpposts ON wpposts.ID = wpcomments.comment_post_ID
     JOIN laraweb.posts AS larawebposts ON larawebposts.slug = wpposts.post_name COLLATE utf8_unicode_ci
   WHERE wpcomments.comment_parent = 0;
+
+-- Insert comments with parents
+
+INSERT INTO laraweb.comments (id, post_id, user_id, parent, author_name, author_email, author_url, body, upvotes, downvotes, approved, spam, ip, created_at, updated_at)
+  SELECT
+    wpcomments.comment_ID,
+    larawebposts.id,
+    NULL ,
+    IF(wpcomments.comment_parent = 0, NULL, wpcomments.comment_parent),
+    wpcomments.comment_author,
+    wpcomments.comment_author_email,
+    wpcomments.comment_author_url,
+    wpcomments.comment_content,
+    0,
+    0,
+    IF(wpcomments.comment_approved = 'spam', 0, 1),
+    IF(wpcomments.comment_approved = 'spam', 1, 0),
+    wpcomments.comment_author_IP,
+    wpcomments.comment_date,
+    wpcomments.comment_date
+  FROM wordpressdb.wp_comments AS wpcomments
+    JOIN wordpressdb.wp_comments AS wpcommentsparent ON wpcomments.comment_parent = wpcommentsparent.comment_ID
+    JOIN wordpressdb.wp_posts AS wpposts ON wpposts.ID = wpcomments.comment_post_ID
+    JOIN laraweb.posts AS larawebposts ON larawebposts.slug = wpposts.post_name COLLATE utf8_unicode_ci
+  WHERE wpcomments.comment_parent <> 0 ORDER BY wpcomments.comment_date ASC;
